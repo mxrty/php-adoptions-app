961ee17d710efd9edb32ba771b7d207d2746af94
Marty67 <martynas67@gmail.com>
2019-05-01 23:54:57 +0100

    update database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -13,8 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-
+    'default' => env('DB_CONNECTION', 'your_heroku_mysql_connection'),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -84,6 +90,17 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+
+        'your_heroku_mysql_connection' => array(
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ),
 
     ],
 
